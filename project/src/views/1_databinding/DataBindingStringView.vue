af8030e94682b3e7bf8f3ec91ccf5ab6dd726d1e
Kim Hee ju <jju95@gmail.com>
2023-03-01 23:36:59 +0900

    data binding

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Hello {{ userName }}</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      message: 'Wellcome vue 실습~!',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+<style scoped>
+.text-red {
+    color: red;
+}
+</style>
